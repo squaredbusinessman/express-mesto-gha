f2723d3c2a83b8778c3ff1bfefd75b200a545612
squaredbusinessman <burdaonelove@gmail.com>
2022-08-17 20:31:56 +0300

    feat: Создает схемы и модели (user, card)

---
+++ models/card.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
