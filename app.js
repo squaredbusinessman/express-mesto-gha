637a97136104956a277377a001c316134c1b0501
squaredbusinessman <burdaonelove@gmail.com>
2022-08-17 19:55:10 +0300

    feat: Подключается к mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  userNewUrlParse: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`)
-})
+});
