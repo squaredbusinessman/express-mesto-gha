6077d8c0e5c22fce229ba866322fa276e9fe75ea
squaredbusinessman <burdaonelove@gmail.com>
2022-08-26 17:27:00 +0300

    fix: controllers-user v3.2

--- controllers/users.js
+++ controllers/users.js
@@ -28,8 +28,8 @@ const getUser = (req, res) => {
     .catch((err) => {
       if (err.name === 'CastError') {
         res.status(400).send(err.message);
-      } else if (err.statusCode === 404) {
-        res.status(err.statusCode).send(err.message);
+      } else if (err.name === 'UserNotFound') {
+        res.status(404).send(err.message);
       } else {
         res.status(500).send(new ApplicationError());
       }
@@ -47,13 +47,9 @@ const getUsers = (req, res) => {
 };
 
 const updateUserInfo = (req, res) => {
-  console.log(req.params);
   User.findByIdAndUpdate(req.user._id, {
     name: req.body.name,
     about: req.body.about,
-  }, {
-    runValidators: true,
-    new: true,
   }).orFail(() => {
     throw new IncorrectDataSent('обновления информации пользователя');
   })
@@ -63,8 +59,8 @@ const updateUserInfo = (req, res) => {
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send(err.message);
-      } else if (err.statusCode === 404) {
-        res.status(err.statusCode).send(err.message);
+      }  else if (err.name === 'UserNotFound') {
+        res.status(404).send(err.message);
       } else {
         res.status(500).send(ApplicationError());
       }
@@ -74,9 +70,6 @@ const updateUserInfo = (req, res) => {
 const updateAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, {
     avatar: req.body.avatar,
-  }, {
-    runValidators: true,
-    new: true,
   })
     .orFail(() => {
       throw new IncorrectDataSent('обновления аватара');
@@ -87,8 +80,8 @@ const updateAvatar = (req, res) => {
     .catch((err) => {
       if (err.name === 'CastError') {
         res.status(400).send(err.message);
-      } else if (err.statusCode === 404) {
-        res.status(err.statusCode).send(err.message);
+      } else if (err.name === 'UserNotFound') {
+        res.status(404).send(err.message);
       } else {
         res.status(500).send(new ApplicationError());
       }
