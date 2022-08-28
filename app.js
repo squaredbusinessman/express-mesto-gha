bed7373b4860eccf0c452e3c95f5613f8d7963e3
squaredbusinessman <burdaonelove@gmail.com>
2022-08-28 15:19:28 +0300

    feat: app.use - 404

--- app.js
+++ app.js
@@ -22,8 +22,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', usersRouter);
-app.use('/', cardsRouter);
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Данной страницы - не существует!' });
+});
 
 
 
