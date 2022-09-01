ec52ae74d7dc023e3e0777c800dc7e6b3eb6f26b
squaredbusinessman <burdaonelove@gmail.com>
2022-09-01 11:21:32 +0300

    feat: Добавляет email и password к схеме пользователя, устанавливает необходимые пакеты - validator, bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -17,6 +19,37 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Введите email в правильном формате'
+    }
+  },
+  password: {
+    type: String,
+    required: [true, 'Необходимо указать пароль'],
+    minLength: 6,
+  },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject(new Error('Введены неправильные почта или пароль'))
+    }
+
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error('Введены неправильные почта или пароль'));
+      }
+
+      return user;
+      // если все проверки пройдены - возвращаем пользователя
+    })
+  })
+}
 
 module.exports = mongoose.model('user', userSchema);
