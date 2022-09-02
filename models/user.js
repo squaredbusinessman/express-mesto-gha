9f8db777a9a4479030426e18c1e72144cd2f2b72
squaredbusinessman <burdaonelove@gmail.com>
2022-09-02 15:53:11 +0300

    feat: Реализует необходимую обработку хэша пароля

--- models/user.js
+++ models/user.js
@@ -32,11 +32,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Необходимо указать пароль'],
     minLength: 6,
+    select: false,
   },
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Введены неправильные почта или пароль'))
