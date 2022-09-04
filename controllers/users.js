94bf7a30bc94155160289c9fdad024d3f07a2b8e
squaredbusinessman <burdaonelove@gmail.com>
2022-09-04 19:40:59 +0300

    fix: борьба с тестами 1.4

--- controllers/users.js
+++ controllers/users.js
@@ -145,13 +145,7 @@ const login = (req, res, next) => {
 
           const token = jwt.sign({ _id: user._id }, 'very-hard-key', { expiresIn: '7d' });
 
-          res
-            .cookie('jwt', token, {
-              maxAge: 3600000 * 24 * 7,
-              httpOnly: true,
-              sameSite: true,
-            })
-            .send({ token });
+          res.send({ token });
         })
         .catch(next);
     })
