f532ec460c71837ec57bcc0c3d5887fb0ca7af35
squaredbusinessman <burdaonelove@gmail.com>
2022-09-04 19:46:09 +0300

    fix: Корректирует работу celebrate в роутах юзеров

--- routes/users.js
+++ routes/users.js
@@ -8,26 +8,27 @@ const {
   updateUserInfo,
 } = require('../controllers/users');
 
-router.get('/', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().min(2).max(200).required(),
-  }).unknown(true),
-}), getUsers);
+const avatarRegExp = new RegExp('^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w.-]+)+[\\w\\-._~:/?#[\\]@!$&\'()*+,;=.]+$');
+
+router.get('/', getUsers);
 router.get('/me', getUserData);
-router.get('/:id', getUser);
-router.patch('/me', celebrate({
+router.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().length(24),
+  }),
+}), getUser);
+router.patch('/me',  celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    about: Joi.string().min(2).max(30).required(),
-  }).unknown(true),
-  headers: Joi.object().keys({
-    authorization: Joi.string().min(2).max(200).required(),
-  }).unknown(true),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
 }), updateUserInfo);
 router.patch('/me/avatar', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().min(2).max(200).required(),
-  }).unknown(true),
+  body: Joi.object().keys({
+    avatar: Joi.string()
+      .pattern(avatarRegExp)
+      .message('Поле "avatar" должно быть валидным url-адресом'),
+  }),
 }), updateAvatar);
 
 module.exports = router;
