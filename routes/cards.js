d11f2cce3aa19e3753dc5a29e8b33d05618c1999
squaredbusinessman <burdaonelove@gmail.com>
2022-09-05 13:21:16 +0300

    feat: добавляет celebrate & Joi проыерки при удалении и лайках карточки

--- routes/cards.js
+++ routes/cards.js
@@ -17,8 +17,20 @@ router.post('/', celebrate({
       .regex(/https?:\/\/(www)?(\.)?[0-9а-яa-zё]{1,}\.[а-яa-zё]{2,4}[a-zа-яё\-._~:/?#[\]@!$&'()*+,;=]*#?/i),
   }).unknown(true),
 }), createCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().length(24),
+  }),
+}),deleteCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().length(24),
+  }),
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = router;
