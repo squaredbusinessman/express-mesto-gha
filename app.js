cb0cf576603df8e359d15ba148b3be7bbdbe9c53
squaredbusinessman <burdaonelove@gmail.com>
2022-09-16 08:27:03 +0300

    feat: Создаёт логгер для запросов и ошибок бэкенда

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { errors } = require('celebrate');
 const cors = require('cors');
 const errorHandler = require('./middlewares/error');
 const routes = require('./routes/index');
+const { requestLogDealer, errorLogDealer } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -30,8 +31,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogDealer);
 app.use(routes);
+
+app.use(errorLogDealer);
 app.use(errors());
 app.use(errorHandler);
+
 app.listen(PORT, () => {
 });
