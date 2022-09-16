a4a974d03ad16f4752227bf6d6f9db7ccf38bc60
squaredbusinessman <burdaonelove@gmail.com>
2022-09-16 08:02:33 +0300

    feat: Подключает корсы для локальной дружбы фронта и бэка

--- app.js
+++ app.js
@@ -2,12 +2,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const errorHandler = require('./middlewares/error');
 const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'http://localhost:3001',
+    'https://localhost:3001',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(options));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
