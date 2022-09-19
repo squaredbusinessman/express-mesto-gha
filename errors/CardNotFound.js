4386e7488df92e174d9e9dfa6ac6cfa4835ced81
squaredbusinessman <burdaonelove@gmail.com>
2022-09-19 13:33:10 +0300

    hotfix: добавляет классы основным ошибкам и корректирует их использование в коде приложения

--- errors/CardNotFound.js
+++
@@ -1,9 +0,0 @@
-const ApplicationError = require('./ApplicationError');
-
-class CardNotFound extends ApplicationError {
-  constructor() {
-    super(404, 'Карточка не найдена (указан неверный id)');
-  }
-}
-
-module.exports = CardNotFound;
--- errors/UserNotFound.js
+++
@@ -1,9 +0,0 @@
-const ApplicationError = require('./ApplicationError');
-
-class UserNotFound extends ApplicationError {
-  constructor() {
-    super(404, 'Пользователь не найден (указан неверный id)');
-  }
-}
-
-module.exports = UserNotFound;
